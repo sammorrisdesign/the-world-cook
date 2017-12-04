7f33af8e06a14553c9e1a54cebd20f99ad584f0d
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-12-03 22:02:46 -0500

    track active steps

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,9 @@
 window.$ = require('./vendors/jquery');
 
 if ($('body').attr('data-page-type') === 'recipe') {
+    var recipes = require('./partials/recipes');
     var ingredients = require('./partials/ingredients');
 
+    recipes.init();
     ingredients.init();
 }
---
+++ src/javascripts/partials/recipes.js
@@ -0,0 +1,35 @@
+var activeStep = 0,
+    scrollTop = 0;
+
+module.exports = {
+    init: function() {
+        this.bindings();
+    },
+
+    bindings: function() {
+        $(window).scroll(function() {
+            this.onScroll();
+        }.bind(this));
+
+        $(window).resize(function() {
+            this.onScroll();
+        }.bind(this));
+    },
+
+    onScroll: function() {
+        this.updateScrollTop();
+        this.setActiveStep();
+    },
+
+    updateScrollTop: function() {
+        scrollTop = $(window).scrollTop();
+    },
+
+    setActiveStep: function() {
+        $('.recipe-step').each(function(i, el) {
+            if (scrollTop > $(el).offset().top - $(el).height() / 2) {
+                activeStep = $(el).data('step');
+            }
+        }.bind(this));
+    }
+}
