a1a250ccb1e5a4bb10c7610b0f86734038ebfc4f
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-05-28 13:22:40 -0400

    add analytics

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,9 +2,11 @@ window.$ = require('./vendors/jquery');
 
 var region = require('./partials/region');
 var filter = require('./partials/filter');
+var analytics = require('./partials/analytics');
 
 region.init();
 filter.init();
+analytics.init();
 
 var pageType = $('body').attr('data-page-type');
 
