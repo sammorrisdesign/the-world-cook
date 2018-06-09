26c87d6632f66856251229c08a2736343775af03
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-06-09 15:10:26 -0400

    design home page

--- src/javascripts/partials/header.js
+++ src/javascripts/partials/header.js
@@ -2,7 +2,7 @@ var d3 = require('d3');
 var planetaryjs = require('planetary.js');
 var planetJSON = require('../../../node_modules/planetary.js/dist/world-110m.json');
 
-var planet, canvas, width, height;
+var planet, canvas, width, height, context;
 
 module.exports = {
     init: function() {
@@ -20,8 +20,10 @@ module.exports = {
 
     setCanvasSize: function() {
         canvas = document.getElementsByClassName('home-header__canvas')[0];
-        width = $('.home-header').width();
-        height = $('.home-header').height() - 80;
+        width = $('.home-header__canvas').width();
+        height = $('.home-header__canvas').height();
+        context = canvas.getContext('2d');
+        context.scale(2, 2);
     },
 
     createGlobe: function() {
@@ -33,37 +35,68 @@ module.exports = {
             borders:  { stroke: '#fff', lineWidth: 1 }
         }));
 
-        planet.projection.scale(height / 2).translate([width / 2, height / 2]).rotate([0, -10, 0]);
-        planet.loadPlugin(this.rotate(10));
+        planet.loadPlugin(this.drawBorder());
+        planet.loadPlugin(this.drawGraticules());
+        planet.loadPlugin(this.rotateToPoint(50));
+        planet.projection.scale(height / 2).translate([width, height / 2]).rotate([0, -10, 0]);
 
         $('.home-header__canvas').attr('width', width).attr('height', height);
         planet.draw(canvas);
     },
 
-    rotate: function(degPerSec) {
+    drawBorder: function() {
         return function(planet) {
-          var lastTick = null;
-          var paused = false;
-          planet.plugins.autorotate = {
-            pause:  function() { paused = true;  },
-            resume: function() { paused = false; }
-          };
-          // ...and configure hooks into certain pieces of its lifecycle.
-          planet.onDraw(function() {
-            if (paused || !lastTick) {
-              lastTick = new Date();
-            } else {
-              var now = new Date();
-              var delta = now - lastTick;
-              // This plugin uses the built-in projection (provided by D3)
-              // to rotate the globe each time we draw it.
-              var rotation = planet.projection.rotate();
-              rotation[0] += degPerSec * delta / 1000;
-              if (rotation[0] >= 180) rotation[0] -= 360;
-              planet.projection.rotate(rotation);
-              lastTick = now;
-            }
-          });
-        };
+            planet.onDraw(function() {
+                planet.withSavedContext(function(context) {
+                    context.beginPath();
+                    planet.path.context(context)({type: 'Sphere'});
+
+                    context.strokeStyle = '#fff';
+                    context.lineWidth = 1;
+                    context.stroke();
+                });
+            });
+        }
+    },
+
+    drawGraticules: function() {
+        return function(planet) {
+            planet.onDraw(function() {
+                planet.withSavedContext(function(context) {
+                    var graticule = d3.geo.graticule().step([20, 20]);
+                    context.beginPath();
+                    planet.path.context(context)(graticule());
+                    context.strokeStyle = '#fff';
+                    context.lineWidth = 1;
+                    context.stroke();
+                });
+            });
+        }
+    },
+
+    rotateToPoint: function(point) {
+        return function(planet) {
+            var lastTick = null;
+            var currentRotation = null;
+
+            planet.onDraw(function() {
+                if (currentRotation === point || !lastTick) {
+                    lastTick = new Date();
+                } else {
+                    var now = new Date();
+                    var delta = now - lastTick;
+
+                    // rotate other direction if this is better
+                    var rotation = planet.projection.rotate();
+                        rotation[0] += 10 * delta / 1000;
+                    if (rotation[0] >= 180) rotation[0] -= 360;
+
+                    currentRotation = Math.floor(rotation[0]);
+
+                    planet.projection.rotate(rotation);
+                    lastTick = now;
+                }
+            });
+        }
     }
 }
