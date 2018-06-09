ac06f3fc16084e0ea013a0261fdbdac5f9f3ca9a
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-06-09 13:17:28 -0400

    responsive header

--- src/javascripts/partials/header.js
+++ src/javascripts/partials/header.js
@@ -2,18 +2,29 @@ var d3 = require('d3');
 var planetaryjs = require('planetary.js');
 var planetJSON = require('../../../node_modules/planetary.js/dist/world-110m.json');
 
-var planet;
+var planet, canvas, width, height;
 
 module.exports = {
     init: function() {
+        this.setCanvasSize();
         this.createGlobe();
+        this.bindings();
+    },
+
+    bindings: function() {
+        $(window).resize(function() {
+            this.setCanvasSize();
+            this.createGlobe();
+        }.bind(this));
+    },
+
+    setCanvasSize: function() {
+        canvas = document.getElementsByClassName('home-header__canvas')[0];
+        width = $('.home-header').width();
+        height = $('.home-header').height() - 80;
     },
 
     createGlobe: function() {
-        var canvas = document.getElementsByClassName('home-header__canvas')[0];
-        var width = $('.home-header__canvas').width();
-        var height = $('.home-header__canvas').height();
-
         planet = planetaryjs.planet();
         planet.loadPlugin(planetaryjs.plugins.earth({
             topojson: { file: 'assets/data/world-110m.json' },
@@ -24,8 +35,6 @@ module.exports = {
 
         planet.projection.scale(height / 2).translate([width / 2, height / 2]).rotate([0, -10, 0]);
         planet.loadPlugin(this.rotate(10));
-
-
 
         $('.home-header__canvas').attr('width', width).attr('height', height);
         planet.draw(canvas);
