f95efcbc1957f2c9c6060cdcce0ad80aafdbaa8f
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-06-09 20:11:41 -0400

    have dates actually work on publishing

--- scripts/data.js
+++ scripts/data.js
@@ -198,10 +198,10 @@ function createRelated(data) {
 function addScheduleStatus(data) {
     for (var i in data) {
 //         var currentDate = new Date();
-        var currentDate = new Date('June 30 2018');
+        var currentDate = new Date('June 11 2018');
         var publishDate = new Date('June ' + data[i].date + ' 2018');
 
-        data[i].isScheduled = publishDate > currentDate || publishDate == currentDate;
+        data[i].isScheduled = currentDate >= publishDate;
     }
 
     return data;
