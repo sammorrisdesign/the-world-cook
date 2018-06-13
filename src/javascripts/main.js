d398539647ddca365cafe8e374bbabbdd65117e8
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-06-13 19:47:29 -0400

    fix analytics

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,9 @@ window.$ = require('./vendors/jquery');
 
 var region = require('./partials/region');
 var filter = require('./partials/filter');
-var analytics = require('./partials/analytics');
 
 region.init();
 filter.init();
-analytics.init();
 
 var pageType = $('body').attr('data-page-type');
 
